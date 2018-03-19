145b8e513231b73991f48baa071c1023d2dd112a
Simon Sanschagrin <simon.sanschagrin@mapaq.gouv.qc.ca>
2018-03-18 21:10:34 -0400

    changement des valeurs retournées

--- POC.Services.HelloWorld/POC.Services.HelloWorld/Controllers/ValuesController.cs
+++ POC.Services.HelloWorld/POC.Services.HelloWorld/Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace POC.Services.HelloWorld.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "Hello", "World!" };
         }
 
         // GET api/values/5
