7e8f0ffa60ec1f390e4817dcd994aabbe9eb680d
Simon Sanschagrin <simon.sanschagrin@mapaq.gouv.qc.ca>
2018-03-19 23:35:31 -0400

    a

--- POC.Services.HelloWorld/POC.Services.HelloWorld/Controllers/ValuesController.cs
+++ POC.Services.HelloWorld/POC.Services.HelloWorld/Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace POC.Services.HelloWorld.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "Hello", "World!", "test8" };
+            return new string[] { "Hello", "World!", "test9" };
         }
 
         // GET api/values/5
